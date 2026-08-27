ff0fd0662d84b9904f90da23209794bf557c3d9b
muthuniramurugan-stack <muthuniramurugan@gmail.com>
2026-08-27 22:35:06 +0530

    added

--- LinkedList/MergeList.java
+++ LinkedList/MergeList.java
@@ -1,22 +1,18 @@
-// Merge Two Sorted List
+// Merge Two Sorted Lists
 package LinkedList;
-
 import java.util.Scanner;
-
 class Node {
     int data;
     Node next;
-
     Node(int data) {
         this.data = data;
         this.next = null;
     }
 }
-
 public class MergeList {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        // LinkedList 01
+        // -------- linkedList 01 --------
         System.out.print("Enter number of nodes for List 1: ");
         int n1 = scan.nextInt();
         Node head1 = null;
@@ -33,7 +29,7 @@ public class MergeList {
                 current1 = newnode;
             }
         }
-        // LinkedList 02
+        // -------- LinkedList 02 --------
         System.out.print("Enter number of nodes for List 2: ");
         int n2 = scan.nextInt();
         Node head2 = null;
@@ -50,7 +46,7 @@ public class MergeList {
                 current2 = newnode;
             }
         }
-        // print first linkedlist
+        // -------- Print List 1 --------
         System.out.println("List 1:");
         current1 = head1;
         while (current1 != null) {
@@ -58,13 +54,58 @@ public class MergeList {
             current1 = current1.next;
         }
         System.out.println("null");
-
-        // print secondLinkedList
+        // -------- Print List 2 --------
         System.out.println("List 2:");
         current2 = head2;
         while (current2 != null) {
             System.out.print(current2.data + " -> ");
             current2 = current2.next;
+        }
+        System.out.println("null");
+        // -------- Merge Two Lists --------
+        Node p1 = head1;
+        Node p2 = head2;
+        Node mergedhead = null;
+        Node mergedcurrent = null;
+        // Compare both lists
+        while (p1 != null && p2 != null) {
+            Node newnode;
+            if (p1.data <= p2.data) {
+                newnode = new Node(p1.data);
+                p1 = p1.next;
+            } else {
+                newnode = new Node(p2.data);
+                p2 = p2.next;
+            }
+            // Add node to the new merged list
+            if (mergedhead == null) {
+                mergedhead = newnode;
+                mergedcurrent = newnode;
+            } else {
+                mergedcurrent.next = newnode;
+                mergedcurrent = newnode;
+            }
+        }
+        // -------- Remaining Nodes from List 1 --------
+        while (p1 != null) {
+            Node newnode = new Node(p1.data);
+            mergedcurrent.next = newnode;
+            mergedcurrent = newnode;
+            p1 = p1.next;
+        }
+        // -------- Remaining Nodes from List 2 --------
+        while (p2 != null) {
+            Node newnode = new Node(p2.data);
+            mergedcurrent.next = newnode;
+            mergedcurrent = newnode;
+            p2 = p2.next;
+        }
+        // -------- Print Merged List --------
+        System.out.println("Merged List:");
+        Node current = mergedhead;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
         }
         System.out.println("null");
     }
