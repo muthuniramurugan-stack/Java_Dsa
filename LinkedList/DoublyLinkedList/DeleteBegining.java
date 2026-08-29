d9d8df7b26036cfe01e60de7e136761705f783c8
muthumurugan272006 <selvikathir.kathiselvikathir@gmail.com>
2026-08-29 06:50:52 +0530

    added

---
+++ LinkedList/DoublyLinkedList/DeleteBegining.java
@@ -0,0 +1,45 @@
+package DoublyLinkedList;
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+public class DeleteBegining {
+    public static void main(String[] args) {
+  Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the Number of Nodes");
+        int n = scan.nextInt();
+        Node head = null;
+        Node current = null;
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter the value");
+            int value = scan.nextInt();
+            Node newnode = new Node(value);
+            if (head == null) {
+                head = newnode;
+                current = newnode;
+            } else {    
+                current.next = newnode;
+                newnode.prev=current;
+                current = newnode;
+            }
+        }
+        System.out.println("Before Deleting Element");
+        current = head;
+        while (current != null) {
+            System.out.print(current.data + "<->");
+            current = current.next;
+        }
+        System.out.println("null");
+        
+        }
+    }
+
