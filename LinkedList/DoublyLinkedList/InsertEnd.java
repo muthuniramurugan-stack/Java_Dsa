c1b5f0d823e847e70f14ef7aca563c881d575819
muthumurugan272006 <selvikathir.kathiselvikathir@gmail.com>
2026-08-29 06:40:37 +0530

    added

---
+++ LinkedList/DoublyLinkedList/InsertEnd.java
@@ -0,0 +1,57 @@
+package DoublyLinkedList;
+import java.util.Scanner;
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+public class InsertEnd {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the Number of Nodes");
+        int n = scan.nextInt();
+        Node head = null;
+        Node current = null;
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter the value");
+            int value = scan.nextInt();
+            Node newnode = new Node(value);
+            if (head == null) {
+                head = newnode;
+                current = newnode;
+            } else {    
+                current.next = newnode;
+                newnode.prev=current;
+                current = newnode;
+            }
+        }
+        current = head;
+        while (current != null) {
+            System.out.print(current.data + "<->");
+            current = current.next;
+        }
+        System.out.println("null");
+        current=head;
+        while(current.next!=null){
+            current=current.next;
+        }
+       System.out.print("Enter the new value");
+       int val = scan.nextInt();
+       Node n1 = new Node(val);
+       current.next=n1;
+       n1.prev=current;
+       n1.next=null;
+       current=head;
+       while(current!=null){
+        System.out.print(current.data+"<->");
+        current=current.next;
+       }
+       System.out.print("null");
+    }
+}
